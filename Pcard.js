359d92e3f9c683826dbfa7067e9abef763315785
Ram Mathala <98813593+rammathala@users.noreply.github.com>
2023-10-24 19:46:13 +0530

    Update Pcard.js

--- Pcard.js
+++ Pcard.js
@@ -1,15 +1,15 @@
 import React, { useState } from 'react'
 import InsertLinkIcon from '@mui/icons-material/InsertLink';
 import './Pcard.css'
-function Pcard({title,image,timestamp,desc,links}) {
+function Pcard({title,image,timestamp,desc,links,stitle}) {
   const tstamps = new Date(timestamp?.toDate()).toLocaleString() /*question mark verify the date then only it works */
   return (
    < div className='Pcards'>
     <h1>{title}</h1>
-    <p className='subtitle'>The Army has accused the Pakistan Army of violating the ceasefire agreement and opening fire during an anti-infiltration operation in the Uri Sector</p>
+    <h3>{stitle}</h3>
  <h5 className='date'>{tstamps}</h5>
 <img src={image} alt='Image'/>
-<h5>Image Name</h5>
+<h5>Image: {title}</h5>
 <p className='descr'>{desc}</p>
 <a   href={links}>Related articles<InsertLinkIcon/></a>
 </div>
