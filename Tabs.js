e42f7ebd5314eba36147e59e2ffd0fdfa7111d44
Ram Mathala <98813593+rammathala@users.noreply.github.com>
2023-10-24 19:48:55 +0530

    Update Tabs.js

--- Tabs.js
+++ Tabs.js
@@ -4,14 +4,14 @@ import { NavLink } from 'react-router-dom'
 function Tabs() {
   return (
     <div className='Tabs'>
-        <NavLink to={'/Local'}>Local News</NavLink>
-        <NavLink to={'/Agriculture'}>Agriculture</NavLink>
-        <NavLink to={'/Business'}>Business</NavLink>
-        <NavLink to={'/Sports'}>Sports</NavLink>
-        <NavLink to={'/Cinemas'}>Cinema</NavLink>
-        <NavLink to={'/Education'}>Education</NavLink>
-        <NavLink to={'/Political'}>Political</NavLink>
-        <NavLink to={'/Cultural'}>Cultural</NavLink>
+        <NavLink to={'/స్థానిక వార్తలు'}>స్థానిక వార్తలు</NavLink>
+        <NavLink to={'/వ్యవసాయం'}>వ్యవసాయం</NavLink>
+        <NavLink to={'/వ్యాపారం'}>వ్యాపారం</NavLink>
+        <NavLink to={'/క్రీడలు'}>క్రీడలు</NavLink>
+        <NavLink to={'/చిత్ర సీమ'}>చిత్ర సీమ</NavLink>
+        <NavLink to={'/విద్య'}>విద్య</NavLink>
+        <NavLink to={'/రాజకీయం'}>రాజకీయం</NavLink>
+        <NavLink to={'/సాంస్కృతి'}>సాంస్కృతి</NavLink>
     </div>
   )
 }
